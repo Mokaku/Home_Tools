989308b890f7a99aa7e7bdcac61a006ac92fc583
Hirofumi.Tobe <mokaku@gmail.com>
2024-11-01 13:50:21 +0900

    Modify and Add Script Oitput html and multi title crowl #2
    少し修正
    ノベルのリスト取得スクリプトで取得フラグ判定を行うように修正

---
+++ Crawling_Web_site/get_narou_work_list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+list_dir="../../novel_database/"
+list_file="narou_all_novel_id.list"
+
+cat ${list_dir}${list_file} |\
+	while read line;
+		do \
+			  ncode_id=$(echo ${line} | cut -d , -f 1)
+			  ncode_get_flg=$(echo ${line} | cut -d , -f 2)
+			  ncode_ref=$(echo ${line} | cut -d , -f 3)
+			# echo $line
+			# echo ${ncode_id}
+			# echo ${ncode_ref}
+			echo "GET_FLAG: ${ncode_get_flg}"
+			if [ ${ncode_get_flg} == 1 ]; then
+				python get_narou_TableOfContents.py ${ncode_id};
+			else
+				echo "PASS: ${ncode_ref}"
+			fi
+			
+		done
